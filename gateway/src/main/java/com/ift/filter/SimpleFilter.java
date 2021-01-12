270836f3aefce9ce01b43953e711007a7fcd6a23
ift <1987047448@qq.com>
2021-01-12 18:44:16 +0800

    Add Sleuth

--- gateway/src/main/java/com/ift/filter/SimpleFilter.java
+++ gateway/src/main/java/com/ift/filter/SimpleFilter.java
@@ -2,6 +2,12 @@ package com.ift.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -9,10 +15,22 @@ import reactor.core.publisher.Mono;
  * @author liufei
  * @date 2021/1/8 14:51
  */
+@Component
+@Order(Integer.MIN_VALUE)
 public class SimpleFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+        String order = exchange.getRequest().getQueryParams().getFirst("order");
+        assert order != null;
+        if (Integer.parseInt(order) < 10) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            DataBuffer dataBuffer = response.bufferFactory()
+                    .wrap("{\"code\":500,\"message\":\"网关拦截\"}".getBytes());
+            return response.writeWith(Mono.just(dataBuffer));
+        }
+        return chain.filter(exchange);
     }
 }
