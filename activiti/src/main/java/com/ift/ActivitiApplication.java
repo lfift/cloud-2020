783dfde204e8bc938bbc135b892becd2fcee58a0
ift <1987047448@qq.com>
2021-01-13 17:59:00 +0800

    Add Activiti

---
+++ activiti/src/main/java/com/ift/ActivitiApplication.java
@@ -0,0 +1,18 @@
+package com.ift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Activiti 工作流引擎
+ *
+ * @author liufei
+ * @date 2021/1/13 15:16
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+}
