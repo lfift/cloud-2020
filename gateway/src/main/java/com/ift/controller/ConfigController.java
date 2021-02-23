d9bac2b6ba79e9e4acef11b3be8d78c81f44293a
ift <1987047448@qq.com>
2021-02-23 18:01:00 +0800

    增加接口校验权限示例

--- gateway/src/main/java/com/ift/controller/ConfigController.java
+++ gateway/src/main/java/com/ift/controller/ConfigController.java
@@ -3,6 +3,7 @@ package com.ift.controller;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class ConfigController {
     @Value("${environment}")
     private String environment;
 
+    @PreAuthorize("#oauth2.hasScope('requiredScope') || hasAnyRole('123')")
     @ApiOperation("测试")
     @GetMapping("/test")
     public String testConfig() {
