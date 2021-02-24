1a29d4de18b886797e6cffa8eeaa68aa65028e51
ift <1987047448@qq.com>
2021-02-24 18:15:05 +0800

    网关整合Security

---
+++ gateway/src/main/java/com/ift/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.ift.config;
+
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+/**
+ * @author liufei
+ * @date 2021/2/24 16:31
+ */
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+}
